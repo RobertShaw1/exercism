098cbb58c114ba170d49e5d049650818f54e2ddc
Robert Shaw <r.shaw1@yahoo.com>
2018-01-09 13:09:23 -0600

    initial commit

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+       if (inputString == "") return "";
+
+       StringBuilder inputStringCopy = new StringBuilder(inputString);
+
+       return inputStringCopy.reverse().toString();
+
+    }
+  
+}
